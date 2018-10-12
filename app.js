a45fe11c41443bce6df4aa17cf14dd77eed4707d
Suhayb Cabdulahi <sabkacade40@gmail.com>
2018-10-12 08:15:00 +0300

    With json file

--- app.js
+++ app.js
@@ -6,5 +6,9 @@ var argv = yargs.argv
 var command = yargs.argv._[0];
 
 if(command === 'add') {
-    secret.addYourSecret(argv.title);
+    secret.addYourSecret(argv.title, argv.body);
+} else if(command === 'remove') {
+    let msg = secret.removeSecret(argv.title);
+    let msg2 = msg ? "Not removed " : "Note removed"
+    console.log(msg2)
 }
--- notes.js
+++ notes.js
@@ -1,9 +1,50 @@
 var fs = require('fs');
 
-const addYourSecret = (title) => {
-    console.log(title);
+const fetchSecret = () => {
+    try {
+        var lastInfo = fs.readFileSync('./note-data.json');
+        return JSON.parse(lastInfo)
+    } catch(e) {
+        return [];
+    }
+}
+
+
+const addYourSecret = (title, body) => {
+    let data = fetchSecret();
+    let obj = {
+        title,
+        body
+    }
+    const duplicate = data.filter(elem => elem.title === title);
+    if(duplicate.length < 1) {
+        data.push(obj);
+        fs.writeFileSync('./note-data.json', JSON.stringify(data));
+        console.log('data is created');
+        logSecret(obj);
+    } else {
+        console.log('this title used before');
+    }
+}
+const removeSecret = (title) => {
+    let data = fetchSecret();
+
+    const rem = data.filter(elem => elem.title !== title);
+    if(rem.length >= 0) {
+        fs.writeFileSync('./note-data.json', JSON.stringify(rem));
+    }
+    return data.length == rem.length
+}
+
+const logSecret = (title) => {
+    console.log('--------------------');
+    console.log('Title:- ', title.title);
+    console.log('Text:- ', title.body)
+
 }
 
 module.exports = {
-    addYourSecret
+    addYourSecret,
+    removeSecret,
+    logSecret
 }
