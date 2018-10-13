795dd8a6d980116b5b7449f42f09f0e74f148a1d
Suhayb Cabdulahi <sabkacade40@gmail.com>
2018-10-13 23:43:45 +0300

    latest with some commands and help

--- app.js
+++ app.js
@@ -1,14 +1,67 @@
-var secret = require('./notes');
+
+const secret = require('./notes');
 var yargs = require('yargs');
 
-var argv = yargs.argv
+var titleOptions = {
+    describe: "Title of the secret",
+    alias: 't',
+    demand: true
+}
+var bodyOptions = {
+    describe: "the secret ot the text",
+    alias: 'b',
+    demand: true
+}
+var argv = yargs.command("add", 'add a new secret', {
+    title: titleOptions,
+    body: bodyOptions
+})
+.command('list', 'list all your secrets')
+.command('read', 'read a secret', {
+    title: titleOptions
+})
+.command('remove', 'remove a secret', {
+    title: titleOptions
+})
+.command('clear', 'clear all your secrets')
+.help()
+.argv
 
-var command = yargs.argv._[0];
+var command = argv._[0];
 
 if(command === 'add') {
-    secret.addYourSecret(argv.title, argv.body);
+     secret.addYourSecret(argv.title, argv.body);
+    
 } else if(command === 'remove') {
     let msg = secret.removeSecret(argv.title);
-    let msg2 = msg ? "Not removed " : "Note removed"
+    let msg2 = msg ? "Not removed Anythinh" : "Your secret was removed removed"
     console.log(msg2)
+} else if(command === 'read') {
+    let getSec = secret.getSecret(argv.title);
+    if(getSec) {
+     secret.logSecret(getSec);
+    } else {
+        console.log('      ')
+        console.log('Your data not found')
+    }
+} else if(command === 'list') {
+    const all = secret.getAllSecret();
+    if(all.length > 0) {
+        console.log('there is ', all.length, 'secrets')
+        const theData = all.map(elem => secret.logSecret(elem));
+      
+    } else {
+        console.log('                ');
+        console.log('................')
+        console.log("There's no data")
+    }
+    
+    
+} else if(command === 'clear') {
+    let nn = secret.clearAllSecret();
+    console.log(`${nn} secrets removed `)
+} else {
+    console.log('           ')
+    console.log('            ');
+    console.log('command not recognized')
 }
--- notes.js
+++ notes.js
@@ -16,35 +16,64 @@ const addYourSecret = (title, body) => {
         title,
         body
     }
-    const duplicate = data.filter(elem => elem.title === title);
+    const duplicate = data.filter(elem => elem.title.toUpperCase() === title.toUpperCase());
     if(duplicate.length < 1) {
         data.push(obj);
         fs.writeFileSync('./note-data.json', JSON.stringify(data));
+        console.log('        ')
         console.log('data is created');
         logSecret(obj);
     } else {
+        console.log('       ')
         console.log('this title used before');
     }
 }
 const removeSecret = (title) => {
     let data = fetchSecret();
 
-    const rem = data.filter(elem => elem.title !== title);
+    const rem = data.filter(elem => elem.title.toUpperCase() !== title.toUpperCase());
     if(rem.length >= 0) {
         fs.writeFileSync('./note-data.json', JSON.stringify(rem));
     }
-    return data.length == rem.length
+    return data.length === rem.length
 }
 
-const logSecret = (title) => {
+const getSecret = (title) => {
+    let data = fetchSecret();
+    const filt = data.filter(elem => elem.title.toUpperCase() === title.toUpperCase());
+    if(filt.length > 0) {
+        console.log('     ')
+        console.log('       ')
+        console.log('Here is your data:');
+        console.log('          ');
+        return filt[0];
+    }
+}
+
+const getAllSecret = () => {
+    return fetchSecret();
+}
+
+const clearAllSecret = () => {
+    let data = fetchSecret();
+    const cnt = data.length;
+    data.length = 0;
+    fs.writeFileSync('./note-data.json', data)
+    return cnt
+}
+
+const logSecret = (secret) => {
     console.log('--------------------');
-    console.log('Title:- ', title.title);
-    console.log('Text:- ', title.body)
+    console.log('Title:- ', secret.title);
+    console.log('Text:- ', secret.body)
 
 }
 
 module.exports = {
     addYourSecret,
     removeSecret,
+    getSecret,
+    getAllSecret,
+    clearAllSecret,
     logSecret
 }
